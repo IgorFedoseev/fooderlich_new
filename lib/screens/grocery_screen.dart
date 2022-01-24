054ef84c416eea3bca3602da748b3d46749b620c
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-01-24 19:08:27 +0300

    completed to buy tab

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'empty_grocery_screen.dart';
 import 'package:provider/provider.dart';
 import '../models/models.dart';
+import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -14,7 +16,27 @@ class GroceryScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          // TODO 11: Present GroceryItemScreen
+          // 1
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          // 2
+          Navigator.push(
+            context,
+            // 3
+            MaterialPageRoute(
+              // 4
+              builder: (context) => GroceryItemScreen(
+                // 5
+                onCreate: (item) {
+                  // 6
+                  manager.addItem(item);
+                  // 7
+                  Navigator.pop(context);
+                },
+                // 8
+                onUpdate: (item) {},
+              ),
+            ),
+          );
         },
       ),
       // 7
@@ -29,8 +51,7 @@ class GroceryScreen extends StatelessWidget {
       builder: (context, manager, child) {
         // 3
         if (manager.groceryItems.isNotEmpty) {
-          // TODO 25: Add GroceryListScreen
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           // 4
           return const EmptyGroceryScreen();
